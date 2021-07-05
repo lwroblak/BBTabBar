525c8e5e0e8ec8ff854287678726c5f2e83ff019
lukasz_wroblak <lukasz.wroblak@escola.pl>
2021-07-05 13:48:04 +0200

    Initial Commit

---
+++ BBTabBar/BBTabBar.h
@@ -0,0 +1,18 @@
+//
+//  BBTabBar.h
+//  BBTabBar
+//
+//  Created by Łukasz Wróblak on 05/07/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBTabBar.
+FOUNDATION_EXPORT double BBTabBarVersionNumber;
+
+//! Project version string for BBTabBar.
+FOUNDATION_EXPORT const unsigned char BBTabBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBTabBar/PublicHeader.h>
+
+
